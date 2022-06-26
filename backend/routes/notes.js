1a65118a2048fd7d2510e13acd5479be02d2613c
samx03 <sameerbakshi34@gmail.com>
2022-06-26 18:52:26 +0530

    Updating note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -21,7 +21,7 @@ router.post('/addnotes', fetchuser, [
     body('description', 'Description must be atleast 5 characters.').isLength({ min: 5 }),
 ], async (req, res) => {
     try {
-        const {title, description, tag} = req.body;
+        const { title, description, tag } = req.body;
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -40,4 +40,24 @@ router.post('/addnotes', fetchuser, [
     }
 });
 
+//Route 3: Update an existing note using: POST "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    //Create a new note object
+    const newNote = {}
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){ return res.status(404).send("Not Found!")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+    res.json(note);
+});
 module.exports = router;
