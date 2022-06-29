09c06f9d3ddd65184460a386c2a0b9263d907bec
samx03 <sameerbakshi34@gmail.com>
2022-06-30 00:08:19 +0530

    Editing notes

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -34,6 +34,8 @@ const NoteState = (props) => {
       },
       body: JSON.stringify({title, description, tag})
     });
+    const json = await response.json();
+    console.log(json);
 
     const note = [
       {
@@ -57,7 +59,7 @@ const NoteState = (props) => {
         'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJiNzBmZGE2ZjdiMjk3NWE1YmVjY2JmIn0sImlhdCI6MTY1NjI0MzQ5NH0.pL5u9C_cltKGm6zNFYJBJZ8vA1Y0WfE5BBC52LjatmA'
       }
     });
-    const json = response.json();
+    const json = await response.json();
     console.log(json);
     const newNotes = notes.filter((note) => { return note._id !== id });
     setNotes(newNotes);
@@ -67,23 +69,28 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     //API Call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJiNzBmZGE2ZjdiMjk3NWE1YmVjY2JmIn0sImlhdCI6MTY1NjI0MzQ5NH0.pL5u9C_cltKGm6zNFYJBJZ8vA1Y0WfE5BBC52LjatmA'
       },
       body: JSON.stringify({title, description, tag})
     });
+    const json = await response.json();
+    console.log(json);
 
+    let newNotes = JSON.parse(JSON.stringify(notes))
     //Logic to edit a note
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes)
   }
   return (
     <noteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>{props.children}</noteContext.Provider>
