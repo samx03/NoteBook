fd54bb139a8c3c0e3b75b097c75ac57bf248d97f
samx03 <sameerbakshi34@gmail.com>
2022-06-29 23:42:51 +0530

    Added a modal to edit notes

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -5,7 +5,7 @@ function Noteitem(props) {
     const context = useContext(noteContext);
     const { deleteNote } = context;
 
-    const { note } = props;
+    const { note, updateNote } = props;
 
     return (
         <div className='col-md-3'>
@@ -14,7 +14,7 @@ function Noteitem(props) {
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
                     <i className="fa-solid fa-trash mx-2" onClick={() => deleteNote(note._id)}></i>
-                    <i className="fa-solid fa-pencil mx-2"></i>
+                    <i className="fa-solid fa-pencil mx-2" onClick={()=>updateNote(note)}></i>
                 </div>
             </div>
         </div>
